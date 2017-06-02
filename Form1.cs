a58ee798533f2fccfa86d48c51f3b8c0ab92bbf8
WIN_NT\x16j023 <x16j023@chiba-fjb.ac.jp>
2017-06-02 12:19:47 +0900

    keybord処理

--- Form1.cs
+++ Form1.cs
@@ -23,27 +23,84 @@ namespace game
         private void button1_Click(object sender, EventArgs e)
         {
             moveX = +speed;
+            moveY = 0;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             moveY = -speed;
+            moveX = 0;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             moveY = +speed;
+            moveX = 0;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             moveX = -speed;
+            moveY = 0;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             pictureBox1.Left += moveX;
             pictureBox1.Top += moveY;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                moveY = -speed;
+                moveX = 0;
+            }
+            if(e.KeyCode == Keys.Down)
+            {
+                moveY = +speed;
+                moveX = 0;
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                moveX = +speed;
+                moveY = 0;
+            }
+            if(e.KeyCode == Keys.Left)
+            {
+                moveX = -speed;
+                moveY = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                moveY = 0;
+                moveX = 0;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                moveY = 0;
+                moveX = 0;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveX = 0;
+                moveY = 0;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                moveX = 0;
+                moveY = 0;
+            }
         }
     }
 }
